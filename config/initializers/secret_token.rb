656a2f6e29f73e663d5b46bf92f7325378c35727
David Van Der Beek <earlynovrock@gmail.com>
2011-10-05 23:01:41 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CE2Rails::Application.config.secret_token = '8697d1dfe01f58f5e61df105b9d7ea3464fed9304a0aef68e4a59a339074664f77bc50a488103c4bc026a87ea2ac6bd971eec3a33463052ab44c3f589862141f'
